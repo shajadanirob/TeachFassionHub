42b4919a3a388d1f78119eb7b73e910d18c4404f
hossainnirob <shajadanirob@gmail.com>
2023-10-20 15:32:32 +0600

    show the rating in the single product

--- src/Components/ProductCategory/SingleProduct.jsx
+++ src/Components/ProductCategory/SingleProduct.jsx
@@ -20,6 +20,17 @@ const SingleProduct = ({data}) => {
               <p className="text-[#CFAF00]  text-center font-semibold text-xl my-5">${price}</p>
 
 
+              <div className="flex justify-center flex-row items-center justify-start gap-2 text-2xl">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="32" height="32"
+        className="text-[#CFAF00]">
+        <path fill-rule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+            clip-rule="evenodd"></path>
+    </svg>
+    <p className="font-bold text-[#CFAF00]">
+        <span>{rating} / 5</span>
+    </p>
+</div>
                
             </div>
             <Link to={`/details/${_id}`} className="flex justify-center items-center">
